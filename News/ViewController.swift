adde91fce97d858548026b44f793fdcbb5334e2c
Vitaliy Pavlyuk <vitaliy.pavlyuk@sigmaukraine.com>
2018-07-27 21:58:41 +0300

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Vitaliy Pavlyuk on 7/27/18.
+//  Copyright © 2018 Vitaliy Pavlyuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
